23bc95b09e435c12f5339b38ddd423137edb3212
Lab402C <Lab402C@ZCO110PCLU40226>
2025-04-09 20:16:11 -0400

    add: DB CONTEXT

---
+++ terscapasSi/Model/Context/AplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using terscapasSi.Model.Entities;
+
+namespace terscapasSi.Model.Context
+{
+    //AQUI DEFINIMOS EL CONTEXT (ESTRUCUTRA) DE LA BASE DE DATOS
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(
+            DbContextOptions<AplicationDbContext> options): base(options)
+        { 
+
+        }
+        //AQUI INDICAMOS QUE LAS SIGUIENTES CLASES (ENTITIES)
+        //CORRESPONDEN A TABLAS DE LA BBDD
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<User> User { get; set; }
+
+    }
+}
--- terscapasSi/Model/Entities/Categoria.cs
+++ terscapasSi/Model/Entities/Categoria.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace terscapasSi.Model.Entities
 {
+    //DataAnnotations
+    [Table("Categoria")]
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
         public bool Activa {  get; set; }
-
-
-
     }
 }
