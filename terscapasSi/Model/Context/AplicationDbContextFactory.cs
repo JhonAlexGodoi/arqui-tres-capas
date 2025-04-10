ba2e99c35a7417237386e7b0c61a8842f208313d
Lab402C <Lab402C@ZCO110PCLU40226>
2025-04-09 20:18:43 -0400

    add: db context factory

--- terscapasSi/Model/Context/AplicationDbContextFactory.cs
+++ terscapasSi/Model/Context/AplicationDbContextFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,14 @@ using System.Threading.Tasks;
 
 namespace terscapasSi.Model.Context
 {
-    public class AplicationDbContextFactory
+    public class AplicationDbContextFactory : IDesignTimeDbContextFactory<AplicationDbContext>
     {
+        public AplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AppEjemplo;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
+            return new AplicationDbContext(optionsBuilder.Options);
+        }
     }
 }
